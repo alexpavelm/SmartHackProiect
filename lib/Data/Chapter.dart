042420e93502cb5365421d8c5da36fc0618a1332
Tudor <tdumitras98@yahoo.com>
2019-11-02 15:53:31 +0200

    Chapter DataStructure

---
+++ lib/Data/Chapter.dart
@@ -0,0 +1,25 @@
+import 'dart:core';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Chapter {
+
+  final int id;
+  final String text;
+  final String keywords;
+  final DocumentReference reference;
+
+  // Data taken from database about cities
+  Chapter.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['id'] != null),
+        assert(map['text'] != null),
+        assert(map['keywords'] != null),
+
+        id = map['id'],
+        text = map['text'],
+        keywords = map['keywords'];
+
+  Chapter.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+}
